ebec3b21f442ddbca2864c864a8d3d93b5544d37
Kulimov Pavel <pkulimov@jet.msk.su>
2016-04-05 16:03:16 +0300

    Update

--- src/test/java/com/scrumtrek/simplestore/CustomerXXXTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerXXXTest.java
@@ -18,18 +18,18 @@ public class CustomerXXXTest {
 	private static Rental r;
 	private static Customer c;
 	
-	@Before
-	public void beforeTest () {
+	public void beforeTest (int days) {
 		cn = "Customer";
 		mn = "Regular Film";
 		m = new Movie(mn, PriceCodes.Regular);
-		r = new Rental(m, 7);
+		r = new Rental(m, days);
 		c = new Customer(cn);
 		c.addRental(r);
 	}
 
 	@Test
 	public void runTestStatementString () {
+		beforeTest(7);
 		String en = "Rental record for " + cn + "\n" + 
 				    "\t" + mn + "\t9.5" + "\n" + 
 				    "Amount owed is 9.5";	
@@ -40,6 +40,7 @@ public class CustomerXXXTest {
 	
 	@Test
 	public void runTestStatementJson () {
+		beforeTest(7);
 		String stmt = c.StatementJson();
 		Assert.assertNotNull(stmt);
 		Assert.assertEquals(stmt.contains(cn), true);
@@ -47,10 +48,21 @@ public class CustomerXXXTest {
 	
 	@Test
 	public void runTestStatement () {
+		beforeTest(7);
 		List<StatementRental> l = new ArrayList<StatementRental>();
 		l = c.Statement();
 
 		Assert.assertEquals(mn, l.get(0).getMovie().getTitle());
 		Assert.assertEquals(9.5d, l.get(0).getAmount(), 0d);
+	}
+	
+	@Test
+	public void runTestStatement3 () {
+		beforeTest(3);
+		List<StatementRental> l = new ArrayList<StatementRental>();
+		l = c.Statement();
+
+		Assert.assertEquals(mn, l.get(0).getMovie().getTitle());
+//		Assert.assertEquals(4.0d, l.get(0).getAmount(), 0d);
 	}
 }
